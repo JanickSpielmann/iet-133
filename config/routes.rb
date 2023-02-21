091b7a4c2497a5b4fda8a1ea08847b9409fa9001
JanickSpielmann <janick.spielmann@messageme.ch>
2023-02-21 10:12:15 +0100

    Carusell view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'helloworld/index'
-  get 'helloworld', to: 'helloworld#index'
+  #get 'pages/home'
+  root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
